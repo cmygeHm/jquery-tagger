96787ef6ee7051e71fd51c55f0de2ac9b47ade87
gafarov <gafarov@realweb.ru>
2012-12-14 15:53:35 +0400

    First commit.

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="/main.css" />
+	<link rel="stylesheet" type="text/css" href="/jquery.tagger.css" />
+	<script type="text/javascript" src="/jquery-1.7.2.min.js" ></script>
+	<script type="text/javascript" src="/jquery.tagger.js" ></script>
+	<title>НЯШКА — честный, непредвзятый рейтинг печенек</title>
+	<meta name="keywords" content="зачем вам ключевики для роботов" />
+	<meta name="description" content="Да, вы - прожженый СЕО-шник, раз смотрите инфу для роботов =)" />
+	<link rel="shortcut icon" href="./favicon.ico" type="image/x-icon">
+	<link rel="icon" href="/img/favicon.ico" type="image/x-icon">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="language" content="en" />
+
+</head>
+<script>
+$(document).ready(function(){
+	$("lu").tagger();
+});
+</script>
+<body>
+	<form id="loginForm">
+		<lu>
+			<li>Петербург</li>
+			<li>Ленинград</li>
+			<li>Петроградище</li>
+			<li>StPetersburg</li>
+			<li>Санкт-Петербург</li>
+			<li>Северная Пальмира</li>
+			<li>Северная Венеция</li>
+			<li>Столца Культуры</li>
+			<li>Северная Столица</li>
+			<li>Культурная столица</li>
+			<li>СПб</li>
+		</lu>
+		<input type="submit" value="Авторизоваться">
+	</form>
+<!--	<div style="border: 1px solid red; width: 5px; height: 150px; position: absolute; left: 790px; top: 378px">test</div>-->
+</body>
+</html>
---
+++ jquery.tagger.js
@@ -0,0 +1,216 @@
+/*
+gafarov-am@ya.ru
+*/
+(function($){
+	$.fn.tagger = function(){
+
+		return this.each(function(_options){
+
+			var options = {
+				editableArea: {
+					width: '250px',
+					padding: '1px',
+					minHeight: '40px',
+					backgroundColor: 'white',
+					borderColor: 'green',
+					borderStyle: 'solid',
+					borderWidth: '1px'
+				},
+				variantList: {
+					borderColor: 'blue',
+					borderStyle: 'solid',
+					borderWidth: '1px',
+					shiftToRight: 1
+				}
+			}
+
+			$.extend(options, _options);
+			var originalList = {};
+			var i = 0;
+			$(this).children('li').each(function(){
+				originalList[i++] = $(this).text();
+			});
+
+			var backspaceCount = 0;
+			var $input = $('<input type=text class="taggerCommonHeight taggerInput" />')
+				.click(function(){
+					variantList.show();
+				})
+				.keyup(function(ev){
+					var collection = variantList.findOccurenceInOriginalList($(this).val().toLowerCase());
+					variantList.redrawListItems(collection);
+				})
+				.keydown(function(ev){
+					if (ev.which == 188) // код запятой
+					{
+						if ($(this).val().replace(' ', '', 'g').length)
+						{
+							variantList.insert($(this).val());
+							variantList.redrawListItems(originalList);
+						}
+						ev.preventDefault();
+					}
+					else if (ev.which == 8)  // код backspace
+					{
+						if ($(this).val().replace(' ', '', 'g').length == 0)
+							backspaceCount++;
+						if (backspaceCount == 1)
+						{
+							$editableArea.children('.taggerSelectedItem:last').addClass('taggerSelectedItemDark');
+						}
+						else if (backspaceCount == 2)
+						{
+							$editableArea.children('.taggerSelectedItem:last').remove();
+							$editableArea.children('input:last').width(variantList.getFreeSpaceForInput());
+							variantList.redrawListPosition();
+							backspaceCount = 0;
+						}
+					}
+					if (!variantList.isShowed())
+						variantList.show();
+				})
+
+			var $editableArea = $('<div />').css({
+				width: options.editableArea.width,
+				padding: options.editableArea.padding,
+				minHeight: options.editableArea.minHeight,
+				backgroundColor: options.editableArea.backgroundColor,
+				borderColor: options.editableArea.borderColor,
+				borderStyle: options.editableArea.borderStyle,
+				borderWidth: options.editableArea.borderWidth
+			});
+
+			var $newInput = false;
+			$(this).replaceWith($editableArea);
+
+			var variantList = {
+				list: $('<div></div>').css({
+					position: 'absolute',
+					display: 'none',
+					width: ($editableArea.outerWidth() - options.variantList.shiftToRight*2 - parseInt(options.variantList.borderWidth, 10)*2) + 'px',
+					height: '200px',
+					borderColor: options.variantList.borderColor,
+					borderWidth: options.variantList.borderWidth,
+					borderStyle: options.variantList.borderStyle,
+					backgroundColor: 'white',
+					overflowY: 'scroll'
+				}),
+				show: function() {
+					this.redrawListPosition();
+					this.list.css('display', 'block');
+				},
+				isShowed: function() {
+					return (this.list.css('display') == 'block');
+				},
+				hide: function() {
+					this.list.css('display', 'none');
+				},
+				createListItem: function(key, value) {
+
+					var self = this;
+					return $('<div>'+value+'</div>')
+						.addClass('taggerListItem')
+						.attr('variant_list_item_identificator', key)
+						.click(function(){
+							self.insert($(this).html(), $(this).attr('variant_list_item_identificator'));
+							$(this).css('display', 'none');
+							delete originalList[key];
+						});
+				},
+				create: function(items) {
+
+					$newInput = $input.clone(true).width('95%');
+					$editableArea.append($newInput);
+					for(var i in items)
+						this.list.append(this.createListItem(i, items[i]));
+				},
+				redrawListPosition: function() {
+					this.list.css('top',
+						$editableArea.offset().top+
+						$editableArea.outerHeight()
+					);
+					this.list.css('left',
+						$editableArea.offset().left + options.variantList.shiftToRight // сдвиг на 1px вправо под editableArea
+					);
+				},
+				redrawListItems: function(collection) {
+					this.list.html('');
+					for(var i in collection)
+						this.list.append(this.createListItem(i, collection[i]));
+				},
+				insert: function(value, id) {
+					var self = this;
+					var $boxX = $("<span />")
+						.addClass('taggerSelectedItemX')
+						.click(function(){
+							var key = parseInt($(this).closest('div').attr('variant_list_item_identificator'), 10);
+							variantList.list.children('div [variant_list_item_identificator='+(key)+']').css('display', 'block');
+							$(this).closest('div').remove();
+						});
+					$editableArea.children(':last').remove();
+					$editableArea.children(':last').remove()
+					var $selectedItem = $('<div class="taggerCommonHeight taggerSelectedItem">'+value+'</div>');
+					if (id)
+						$selectedItem.attr('variant_list_item_identificator', id);
+					$selectedItem.append($boxX.clone(true));
+					$editableArea.append($selectedItem);
+
+					$newInput = $input.clone(true).width(this.getFreeSpaceForInput());
+					$editableArea.append($newInput);
+
+					$editableArea.append($('<div class="taggerClearBoth"></div>'));
+					$newInput.focus();
+					this.list.hide();
+				},
+				getFreeSpaceForInput: function() {
+
+					var width;
+
+					if ($editableArea.children('.taggerSelectedItem:last').length) {
+						var lastSelectedItemRightBorder =
+							$editableArea.children('.taggerSelectedItem:last').offset().left +
+							$editableArea.children('.taggerSelectedItem:last').width() +
+							parseInt($editableArea.children('.taggerSelectedItem:last').css('padding-right'), 10) +
+							parseInt($editableArea.children('.taggerSelectedItem:last').css('padding-left'), 10);
+
+						var editableAreaRightBorder = $editableArea.offset().left+$editableArea.width();
+						width = editableAreaRightBorder - lastSelectedItemRightBorder - 15;
+						if (width < 15)
+							width = '95%';
+					}
+					else {
+						width = '95%';
+					}
+					
+					return width;
+				},
+				findOccurenceInOriginalList: function(needle) {
+					var result = {};
+					for(var i in originalList) {
+						if (originalList[i].toLowerCase().indexOf(needle) != -1)
+							result[i] = originalList[i];
+					}
+					return result;
+				},
+				deleteInsertedTeg: function($teg) {
+					var key = parseInt($teg.attr('variant_list_item_identificator'), 10);
+					variantList.list.children('div [variant_list_item_identificator='+(key)+']').css('display', 'block');
+					$teg.remove();
+				}
+			};
+
+			variantList.create(originalList);
+			$('body').append(variantList.list);
+
+			// our element blur emulation
+			$(document).click(function(ev){
+				if($newInput[0] != ev.target && variantList.isShowed())
+					variantList.hide();
+			});
+			
+			$(window).resize(function() {
+				variantList.redrawListPosition();
+			});
+		});
+	};
+})(jQuery);
